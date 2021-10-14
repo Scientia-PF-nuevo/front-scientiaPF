a9ee145241fdd497cae0ea1a1b49cb2b6e250e22
Emmanuel <emanuelnavas@hotmail.com>
2021-10-14 19:45:10 -0300

    search bar component and logic

--- scientia/src/index.js
+++ scientia/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './router/App.js';
-import { Provider } from 'react-redux';
-import store from './store/index';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
+import{ Provider} from 'react-redux';
 import store from './store'
-import './firebase'
 
 ReactDOM.render(
   <React.StrictMode>
