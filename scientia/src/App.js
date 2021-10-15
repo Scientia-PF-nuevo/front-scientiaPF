1790e1f0f983aff60fc344317ddc37271939880e
fabios21 <fabsillopez@gmail.com>
2021-10-15 11:41:59 -0500

    Form Sign Up

--- scientia/src/App.js
+++ scientia/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/About/About';
 import Home from './components/Home/Home';
-import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path='/about' component={About} />
-            <Route exact path='/signup' component={SignUp} />
             <Route path='*' component={Page404} />
           </Switch>
       </Router>
