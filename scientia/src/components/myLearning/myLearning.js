e98e23bebcd0fcc784fa4f34045e7be0603bc34b
Emmanuel <emanuelnavas@hotmail.com>
2021-10-21 11:14:20 -0300

    my learning update

--- scientia/src/components/myLearning/myLearning.js
+++ scientia/src/components/myLearning/myLearning.js
@@ -4,36 +4,45 @@ import TrainningCard from '../TrainningCard/TrainningCard'
 import './myLearning.css'
 
 function MyLearning({courses}) {
+
     if (courses.length === 1) {
         var arrCourse = []
         arrCourse.push(courses)
       }
     
-      return courses === "" ? (
-        <div>
-          <h1> NO ENCONTRADO </h1>
+      return (
+        <div className="my-learning-div">
+
+          {
+            courses === "" ? (
+          <div>
+            <h1> SIN CURSOS COMPRADOS </h1>
+          </div>
+        ) : typeof courses !== "undefined" && courses.length >= 1 ? (
+          
+          courses.map((course) => (
+            <TrainningCard
+              key={course.id}
+              id={course.id}
+              name={course.name}
+              score={course.score}
+              date={course.date}
+              price={course.price}
+              url={course.url}
+              categories={course.categories}
+              description={course.description}
+            />
+          ))
+          
+        ) : (
+          <div>
+            <h1>Cargando...</h1>
+          </div>
+        )
+        }
         </div>
-      ) : typeof courses !== "undefined" && courses.length >= 1 ? (
-        
-        courses.map((course) => (
-          <TrainningCard
-            key={course.id}
-            id={course.id}
-            name={course.name}
-            score={course.score}
-            date={course.date}
-            price={course.price}
-            url={course.url}
-            categories={course.categories}
-            description={course.description}
-          />
-        ))
-        
-      ) : (
-        <div>
-          <h1>Cargando...</h1>
-        </div>
-      );
+      
+      )
     }
     
     const mapStateToProps = (state) => {
