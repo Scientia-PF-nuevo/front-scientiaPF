8a1b25c401f76c3765244ac28c391521fa6418cd
Alexis Martinez <alexis4329@gmail.com>
2021-10-22 19:25:27 -0300

    Funcionando con alx-test en back

--- scientia/src/components/myLearning/myLearning.js
+++ scientia/src/components/myLearning/myLearning.js
@@ -4,8 +4,62 @@ import TrainningCard from '../TrainningCard/TrainningCard'
 import {getUserInfo} from '../../actions/actions'
 import './myLearning.css'
 
-function MyLearning({courses,user, getUserInfo}) {
+// function MyLearning({courses,user, getUserInfo}) {
 
+//   useEffect (()=> {
+    
+//     getUserInfo(user.email)
+    
+//   }, [])
+    
+//       return (
+//         <div className="my-learning-div">
+
+//           {
+//             courses === "" ? (
+//           <div>
+//             <h1> SIN CURSOS COMPRADOS </h1>
+//           </div>
+//         ) : typeof courses !== "undefined" && courses.length >= 1 ? (
+          
+//           courses.map((course) => (
+            
+//             <TrainningCard
+//               key={course.courseId}
+//               id={course.courseId}
+//               name={course.state}
+//               score={course.price}
+//               date={course.createdAt}
+//               price={course.price}
+//               // url={course.courseP}
+//               categories={course.owner}
+//               description={course.owner}
+//             />
+
+//           ))
+          
+//         ) : (
+//           <div>
+//             <h1>Cargando...</h1>
+//           </div>
+//         )
+//         }
+//         </div>
+      
+//       )
+//     }
+    
+//     const mapStateToProps = (state) => {
+//         return {
+//             courses: state.rootReducer.userInfo.bought_courses,
+//             user: state.rootReducer.user
+//         }
+//     }
+    
+//     export default connect(mapStateToProps, {getUserInfo})(MyLearning)
+    
+function MyLearning({courses,user, getUserInfo}) {
+  console.log(courses.coursesAndData)
   useEffect (()=> {
     
     getUserInfo(user.email)
@@ -16,25 +70,37 @@ function MyLearning({courses,user, getUserInfo}) {
         <div className="my-learning-div">
 
           {
-            courses === "" ? (
+            courses.coursesAndData.length === 0 ? (
           <div>
             <h1> SIN CURSOS COMPRADOS </h1>
           </div>
-        ) : typeof courses !== "undefined" && courses.length >= 1 ? (
+        ) : courses.coursesAndData.length >= 1 ? (
           
-          courses.map((course) => (
+          courses.coursesAndData.map((c) => {
+            
+            let suma = 0;
+					  let average;
+					  const SCs = c.reviews.map((r, index) => {
+						suma = suma + r.score;						
+						average = suma / index;
+					});
+            console.log(c.url)
+            return (
+            
             <TrainningCard
-              key={course.courseId}
-              id={course.courseId}
-              name={course.state}
-              score={course.price}
-              date={course.createdAt}
-              price={course.price}
-              url="https://www.tallermecanico.com.ar/wp-content/uploads/2018/08/blog10-960x500.jpg"
-              categories={course.owner}
-              description={course.owner}
+              key={c.course.courseId}
+              id={c.course.courseId}
+              name={c.course.courseName}
+              score={average}
+              date={c.course.createdAt}
+              price={c.course.price}
+              url={c.url}
+              urlVideo={c.urlVideo}
+              categories={c.course.owner}
+              description={c.course.description}
             />
-          ))
+
+          )})
           
         ) : (
           <div>
@@ -49,7 +115,7 @@ function MyLearning({courses,user, getUserInfo}) {
     
     const mapStateToProps = (state) => {
         return {
-            courses: state.rootReducer.userInfo.usuario.bought_courses,
+            courses: state.rootReducer.userInfo,
             user: state.rootReducer.user
         }
     }
