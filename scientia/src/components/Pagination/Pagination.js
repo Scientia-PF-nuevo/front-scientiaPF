45906fe9fb33366c3303f4e5a9371eec9c29faf8
Emmanuel <emanuelnavas@hotmail.com>
2021-11-03 09:27:16 -0300

    fix nacho

--- scientia/src/components/Pagination/Pagination.js
+++ scientia/src/components/Pagination/Pagination.js
@@ -4,6 +4,11 @@ import './Pagination.css'
 
 export default function Pagination({ cardPerPage, totalCards, paginate, currentPage }) {
     
+
+    if(Math.ceil(totalCards / cardPerPage ) < currentPage ) {
+      paginate(1)
+    }
+
     const pageNumbers = [];
     for(let i = 1; i <= Math.ceil(totalCards / cardPerPage ); i++){
         pageNumbers.push(i);
