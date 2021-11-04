b976fe31d0ffe9baba3b063b5e29c2690433eeac
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-03 21:25:06 -0300

    Updates and new Form

--- scientia/src/components/NewForm/NewForm5.js
+++ scientia/src/components/NewForm/NewForm5.js
@@ -34,30 +34,16 @@ export default function NewForm5(props) {
         e.preventDefault();
         setCourse({
             ...course,
-            [e.target.name]: e.target.value
+            [e.target.name]: parseInt(e.target.value)
         });
     };
 
     function handleSubmit(e) {
         e.preventDefault();
-        if (typeof course.numbersOfDiscounts !== "number") {
-            return setCourse({
-                ...course,
-                numbersOfDiscounts: 0
-            });
-        }
-        if (typeof course.percentageDiscount !== "number") {
-            return setCourse({
-                ...course,
-                percentageDiscount: 0
-            });
-        }
-
         if (course.numbersOfDiscounts !== 0 && course.percentageDiscount === 0) {
             setMsg('People would not use your discount if percentage is under 0')
             return handleShow();
         }
-
         if (course.percentageDiscount !== 0 && course.numbersOfDiscounts === 0) {
             setMsg('People would not use your discount if the amount of discounts is under 0')
             return handleShow();
@@ -92,29 +78,29 @@ export default function NewForm5(props) {
             <form className='formLast'>
                 <h1>You wanna add a discount?</h1>
                 <br></br>
-                <TextField required 
-                style={{marginBottom:"10px"}}
-                id="outlined-required"
-                label="Percentage"
-                // className='placeHolder' 
-                type="number" 
-                value={course.percentageDiscount}
-                name="percentageDiscount" 
-                autocomplete="off"
-                onChange={e => handleChange(e)} />
+                <select className='selector' placeholder='Percentage...' name="percentageDiscount" value={course.percentageDiscount} onChange={handleChange} >
+                    <option defaultValue="selected">0%</option>
+                    <option name="percentageDiscount" value={10}> 10% </option>
+                    <option name="percentageDiscount" value={15}> 15% </option>
+                    <option name="percentageDiscount" value={20}> 20% </option>
+                    <option name="percentageDiscount" value={25}> 25% </option>
+                    <option name="percentageDiscount" value={30}> 30% </option>
+                    <option name="percentageDiscount" value={35}> 35% </option>
+                </select>
 
-                <h4>AMOUNT OF DISCOUNTS</h4>
+                <h4>Amount of discounts</h4>
                 <br></br>
-                <TextField required 
-                style={{marginBottom:"10px"}}
-                id="outlined-required"
-                label="Amount"
-                // className='placeHolder' 
-                type="number" 
-                value={course.numbersOfDiscounts}
-                name="numbersOfDiscounts" 
-                autocomplete="off"
-                onChange={e => handleChange(e)} />
+                <select className='selector' placeholder='Quantity...' name="numbersOfDiscounts" value={course.numbersOfDiscounts} onChange={handleChange} >
+                    <option defaultValue="selected">0</option>
+                    <option name="numbersOfDiscounts" value={5}> 5 </option>
+                    <option name="numbersOfDiscounts" value={10}> 10 </option>
+                    <option name="numbersOfDiscounts" value={20}> 20 </option>
+                    <option name="numbersOfDiscounts" value={25}> 25 </option>
+                    <option name="numbersOfDiscounts" value={30}> 30 </option>
+                    <option name="numbersOfDiscounts" value={35}> 35 </option>
+                    <option name="numbersOfDiscounts" value={40}> 40 </option>
+                    <option name="numbersOfDiscounts" value={50}> 50 </option>
+                </select>
 
 
             <div className='containerbtSub'>
