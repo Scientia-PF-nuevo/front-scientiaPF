0c36c14dc1bef1913168de0b279ee7ad82127b83
lauty95 <lautaroJ95@gmail.com>
2021-11-04 14:06:57 -0300

    asd

--- scientia/src/components/Cart/Cart.js
+++ scientia/src/components/Cart/Cart.js
@@ -27,47 +27,47 @@ import './Cart.css'
 export function Cart(props) {
 
   useEffect(() => {
-    window.addEventListener('mousemove', () => {});
+    window.addEventListener('mousemove', () => { });
     props.removeAllGift()
     return () => {
-       window.removeEventListener('mousemove', () => {})
+      window.removeEventListener('mousemove', () => { })
     }
- }, [])
+  }, [])
 
-const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
 
-const handleClickVariantOk = () => {
-        enqueueSnackbar('YOUR GIFT ADDED CORRECTLY', {
-          anchorOrigin: {
-              vertical: 'bottom',
-              horizontal: 'left',              
-          },
-          TransitionComponent: Slide,
-          variant: 'success',
-      })
-}
-
-const handleClickVariantWrongEmail = () => {
-  enqueueSnackbar('INCORRECT EMAIL OR MISSED', {
-    anchorOrigin: {
+  const handleClickVariantOk = () => {
+    enqueueSnackbar('YOUR GIFT ADDED CORRECTLY', {
+      anchorOrigin: {
         vertical: 'bottom',
-        horizontal: 'left',              
-    },
-    TransitionComponent: Slide,
-    variant: 'error',
-})
-}
+        horizontal: 'left',
+      },
+      TransitionComponent: Slide,
+      variant: 'success',
+    })
+  }
 
-const handleClickVariantWrongRemovedGift = () => {
-  enqueueSnackbar('ITEM REMOVED', {
-    anchorOrigin: {
+  const handleClickVariantWrongEmail = () => {
+    enqueueSnackbar('INCORRECT EMAIL OR MISSED', {
+      anchorOrigin: {
         vertical: 'bottom',
-        horizontal: 'left',              
-    },
-    TransitionComponent: Slide,
-    variant: 'warning',
-})
-}
+        horizontal: 'left',
+      },
+      TransitionComponent: Slide,
+      variant: 'error',
+    })
+  }
+
+  const handleClickVariantWrongRemovedGift = () => {
+    enqueueSnackbar('ITEM REMOVED', {
+      anchorOrigin: {
+        vertical: 'bottom',
+        horizontal: 'left',
+      },
+      TransitionComponent: Slide,
+      variant: 'warning',
+    })
+  }
 
   const {
     cart,
@@ -92,46 +92,48 @@ const handleClickVariantWrongRemovedGift = () => {
     courseId: 0,
     price: 0,
     orderId: 0,
-    emailGift: "" });
-    
+    emailGift: ""
+  });
+
   const [show2, setShow2] = useState(false);
 
   const handleClose3 = () => {
     setShow2(false)
   };
-  
+
   const handleChange = (event) => {
 
-    if (login){
+    if (login) {
 
       if (cart.length > 0) {
-        var [selectedCourse] = cart.filter((course)=> course.coursesId === parseInt(event.target.name))
+        var [selectedCourse] = cart.filter((course) => course.coursesId === parseInt(event.target.name))
       }
-      
-        setChecked({...checked,
-          gift: event.target.checked,
-          courseId: parseInt(event.target.name),
-          price: selectedCourse.price || 0,
-          orderId: selectedCourse.id || 0,
-          emailGift: "",
-          [event.target.name]:event.target.checked
-        });
-      
-      if(checked[event.target.name]){
-          handleClickVariantWrongRemovedGift()
+
+      setChecked({
+        ...checked,
+        gift: event.target.checked,
+        courseId: parseInt(event.target.name),
+        price: selectedCourse.price || 0,
+        orderId: selectedCourse.id || 0,
+        emailGift: "",
+        [event.target.name]: event.target.checked
+      });
+
+      if (checked[event.target.name]) {
+        handleClickVariantWrongRemovedGift()
       }
-  
+
       if (!checked[event.target.name])
-      setShow2(true)
+        setShow2(true)
       removeGift(parseInt(event.target.name))
     } else {
       history.push("/login")
     }
 
-    
+
   };
 
-  const [state, setState] = React.useState({emailGift: ""})
+  const [state, setState] = React.useState({ emailGift: "" })
 
   function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -139,16 +141,16 @@ const handleClickVariantWrongRemovedGift = () => {
   }
 
   const handleClose2 = (email) => {
-    if(validateEmail(email)) {
+    if (validateEmail(email)) {
       addGift(checked)
       handleClickVariantOk('success')
       setShow2(false)
     } else {
       handleClickVariantWrongEmail('error')
     }
-   
+
   };
-  const handleChangeGift = (e) => setChecked({...checked,[e.target.name]: e.target.value})
+  const handleChangeGift = (e) => setChecked({ ...checked, [e.target.name]: e.target.value })
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
@@ -175,7 +177,7 @@ const handleClickVariantWrongRemovedGift = () => {
     return `$ ${parseFloat(result.toFixed(2))}`;
   }
 
-  if(cart.length >= 1) {
+  if (cart.length >= 1) {
     Total()
   }
 
@@ -225,8 +227,8 @@ const handleClickVariantWrongRemovedGift = () => {
   }
 
   const haddleRemoveItem = (id) => {
-    if(login){
-      const data = {id: id, email: user.email}
+    if (login) {
+      const data = { id: id, email: user.email }
       removeGift(id)
       deleteCartLogged(data)
       handleClickVariantWrongRemovedGift('removed')
@@ -239,140 +241,140 @@ const handleClickVariantWrongRemovedGift = () => {
 
   return (
     <div >
-    <div className="title-cart-div">
-      <h1>Shopping Cart</h1>
-    </div>
-    <div className="wrapper-cart">
-      <div className="cart-div">
-      <div className="shopping-cart-div">
+      <div className="title-cart-div">
+        <h1>Shopping Cart</h1>
       </div>
-       <h3>{cart && cart.length > 0 ? cart.length : 0} Courses in Cart</h3>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th style={{ textAlign: "center" }}>Course</th>
-              <th style={{ textAlign: "center" }}>Course Name</th>
-              <th style={{ textAlign: "center" }}>Price</th>
-              <th style={{ textAlign: "center" }}>Gift a Course</th>
-              <th style={{ textAlign: "center" }}>ID</th>
-              {/* <th style={{ textAlign: "center" }}>Sub-Total</th> */}
-              <th style={{ textAlign: "center" }}>Remove</th>
-            </tr>
-          </thead>
-          {cart.length >= 1 ? (
-            cart.map((course) => (
-              <tbody className="tbody-div">
-                <tr>
-                  <td className="photo-div">
-                    <img className="cart-img" src={course.url} />
-                  </td>
-                  <td style={{ textAlign: "center" }}>
-                    <div className="div-center2">
-                      <Link
-                        to="/details"
-                        onClick={() => addDetails(course.coursesId)}
-                        className="link-div-cart"
-                      >
-                        {course.name && course.name.toUpperCase()}
-                      </Link>
-                    </div>
-                  </td>
-                  <td style={{ textAlign: "center" }}>
-                    {course.percentageDiscount !== null && course.percentageDiscount > 0 ? (
-                      <div>
-                        <h3
-                          style={{
-                            color: "red",
-                            textDecoration: "line-through",
-                          }}
+      <div className="wrapper-cart">
+        <div className="cart-div">
+          <div className="shopping-cart-div">
+          </div>
+          <h3>{cart && cart.length > 0 ? cart.length : 0} Courses in Cart</h3>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th style={{ textAlign: "center" }}>Course</th>
+                <th style={{ textAlign: "center" }}>Course Name</th>
+                <th style={{ textAlign: "center" }}>Price</th>
+                <th style={{ textAlign: "center" }}>Gift a Course</th>
+                <th style={{ textAlign: "center" }}>ID</th>
+                {/* <th style={{ textAlign: "center" }}>Sub-Total</th> */}
+                <th style={{ textAlign: "center" }}>Remove</th>
+              </tr>
+            </thead>
+            {cart.length >= 1 ? (
+              cart.map((course) => (
+                <tbody className="tbody-div">
+                  <tr>
+                    <td className="photo-div">
+                      <img className="cart-img" src={course.url} />
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      <div className="div-center2">
+                        <Link
+                          to="/details"
+                          onClick={() => addDetails(course.coursesId)}
+                          className="link-div-cart"
                         >
-                          ${parseFloat(course.price !== null && course.price.toFixed(2))}
-                        </h3>
-                        <p>{course.percentageDiscount}% OFF</p>
-                        <h3 style={{ color: "green" }}>
-                          $
-                          {parseFloat((course.price -((course.percentageDiscount / 100) * course.price)).toFixed(2)
-                          )}
-                        </h3>
+                          {course.name && course.name.toUpperCase()}
+                        </Link>
                       </div>
-                    ) : (
-                      <div className="div-center">
-                        <h3 style={{ color: "green" }}> ${parseFloat(course.price.toFixed(2))}</h3>
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      {course.percentageDiscount !== null && course.percentageDiscount > 0 ? (
+                        <div>
+                          <h3
+                            style={{
+                              color: "red",
+                              textDecoration: "line-through",
+                            }}
+                          >
+                            ${parseFloat(course.price !== null && course.price.toFixed(2))}
+                          </h3>
+                          <p>{course.percentageDiscount}% OFF</p>
+                          <h3 style={{ color: "green" }}>
+                            $
+                            {course.price !== null && parseFloat((course.price - ((course.percentageDiscount / 100) * course.price)).toFixed(2)
+                            )}
+                          </h3>
+                        </div>
+                      ) : (
+                        <div className="div-center">
+                          <h3 style={{ color: "green" }}> ${course.price !== null && parseFloat(course.price.toFixed(2))}</h3>
+                        </div>
+                      )}
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      <div className="div-center3">
+                        <Checkbox
+                          sx={{ "& .MuiSvgIcon-root": { fontSize: 40 } }}
+                          name={course.coursesId}
+                          checked={
+                            checked.hasOwnProperty(course.coursesId)
+                              ? checked[course.coursesId]
+                              : false
+                          }
+                          onChange={handleChange}
+                          inputProps={{ "aria-label": "controlled" }}
+                        />
+                        <p>GIFT</p>
                       </div>
-                    )}
-                  </td>
-                  <td style={{ textAlign: "center" }}>
-                    <div className="div-center3">
-                      <Checkbox
-                        sx={{ "& .MuiSvgIcon-root": { fontSize: 40 } }}
-                        name={course.coursesId}
-                        checked={
-                          checked.hasOwnProperty(course.coursesId)
-                            ? checked[course.coursesId]
-                            : false
-                        }
-                        onChange={handleChange}
-                        inputProps={{ "aria-label": "controlled" }}
-                      />
-                      <p>GIFT</p>
-                    </div>
-                  </td>
-                  {/* <td style={{ textAlign: "center" }}>
+                    </td>
+                    {/* <td style={{ textAlign: "center" }}>
                     <div className="div-center">
                       <h3 style={{ color: "red" }}>{Total()}</h3>
                     </div>
                   </td> */}
-                  <td style={{ textAlign: "center" }}>
-                    <div className="div-center">
-                      <p>#{course.coursesId}</p>
-                    </div>
-                  </td>
-                  <td style={{ textAlign: "center" }}>
-                    {
+                    <td style={{ textAlign: "center" }}>
                       <div className="div-center">
-                        <DeleteRoundedIcon
-                          onClick={() => haddleRemoveItem(course.coursesId)}
-                          style={{ cursor: "pointer", fontSize: 40 }}
-                        />
+                        <p>#{course.coursesId}</p>
                       </div>
-                    }
-                  </td>
-                </tr>
-              </tbody>
-            ))
-          ) : (
-            <div></div>
-          )}
-        </Table>
-        <div>
-          {gift && gift.length >= 1 ? (
-            gift.map((g) => (
-              <div>
-                <p><strong>COURSE ID :</strong> (#{g.courseId}) - <strong> GIFT TO EMAIL : </strong>{g.emailGift}</p>
-              </div>
-            ))
-          ) : (
-            <p>ANY COURSE HAS BEEN SELECTED FOR GIFT</p>
-          )}
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      {
+                        <div className="div-center">
+                          <DeleteRoundedIcon
+                            onClick={() => haddleRemoveItem(course.coursesId)}
+                            style={{ cursor: "pointer", fontSize: 40 }}
+                          />
+                        </div>
+                      }
+                    </td>
+                  </tr>
+                </tbody>
+              ))
+            ) : (
+              <div></div>
+            )}
+          </Table>
+          <div>
+            {gift && gift.length >= 1 ? (
+              gift.map((g) => (
+                <div>
+                  <p><strong>COURSE ID :</strong> (#{g.courseId}) - <strong> GIFT TO EMAIL : </strong>{g.emailGift}</p>
+                </div>
+              ))
+            ) : (
+              <p>ANY COURSE HAS BEEN SELECTED FOR GIFT</p>
+            )}
+          </div>
+          <p>
+            <strong>SUB - TOTAL:</strong> $ {parseFloat(result.toFixed(2))}
+          </p>
+          <p>
+            <strong>TAXs (21%):</strong> $ {taxs}
+          </p>
+          <p>
+            <strong>TOTAL:</strong> $ {total}
+          </p>
+          <div className="confirm-order-div">
+            <button className="confirm-button" onClick={handledSubmitOrder}>
+              CHECKOUT
+            </button>
+          </div>
+          <br></br>
+          <br></br>
         </div>
-        <p>
-          <strong>SUB - TOTAL:</strong> $ {parseFloat(result.toFixed(2))}
-        </p>
-        <p>
-          <strong>TAXs (21%):</strong> $ {taxs}
-        </p>
-        <p>
-          <strong>TOTAL:</strong> $ {total}
-        </p>
-        <div className="confirm-order-div">
-          <button className="confirm-button" onClick={handledSubmitOrder}>
-            CHECKOUT
-          </button>
-        </div>
-        <br></br>
-        <br></br>
       </div>
-    </div>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
